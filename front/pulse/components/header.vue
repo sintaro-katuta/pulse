ae8660d6deafb6eb1ddc88fccd0084450dc56c69
sintaro-katuta <sintaro.katuta@gmail.com>
2024-08-17 00:24:53 +0900

    first commit

---
+++ front/pulse/components/header.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { NavItem } from '@nuxt/content'
+
+const navigation = inject<Ref<NavItem[]>>('navigation', ref([]))
+
+const links = [{
+  label: 'about',
+  icon: 'i-heroicons-book-open',
+  to: '/about'
+}, {
+  label: 'dashboard',
+  icon: 'i-heroicons-square-3-stack-3d',
+  to: '/dashboard'
+}, {
+  label: 'account',
+  icon: 'i-heroicons-rocket-launch',
+  to: '/account'
+}]
+</script>
+
+<template>
+  <UHeader :links="links">
+    <template #logo>
+      <Logo class="w-auto h-6" />
+    </template>
+
+    <template #right>
+      <UColorModeButton />
+
+      <UButton to="https://github.com/nuxt/ui" target="_blank" icon="i-simple-icons-github" color="gray" variant="ghost" />
+    </template>
+
+    <template #panel>
+      <UNavigationTree :links="mapContentNavigation(navigation)" />
+    </template>
+  </UHeader>
+</template>
---
+++ front/pulse/components/logo.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex items-center gap-3">
+    <img src="/pulseIcon.png" alt="" width="32"/>
+    <h1 class="logo-font text-4xl font-bold dark:text-stone-100">Pulse</h1>
+  </div>
+</template>
+
+<style scoped>
+  /* Component styles */
+  @import url('https://fonts.googleapis.com/css2?family=Song+Myung&display=swap');
+  .logo-font {
+    font-family: "Song Myung", sans-serif;
+  }
+</style>
