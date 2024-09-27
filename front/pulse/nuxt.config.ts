3651175461c17b0ec99abf613d8b97cba72493bf
勝田紳太郎 <113318875+sintaro-katuta@users.noreply.github.com>
2024-09-28 03:56:00 +0900

    Revert "gitignoreを作成db配下を除外　db環境構築"

--- front/pulse/nuxt.config.ts
+++ front/pulse/nuxt.config.ts
@@ -1,28 +1,7 @@
-import { defineNuxtConfig } from 'nuxt/config'
-import { resolve } from 'path'
-
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   extends: ['@nuxt/ui-pro'],
-  modules: [
-    '@nuxt/ui',
-    '@nuxt/content'
-  ],
-  alias: {
-    '@': resolve(__dirname, './components'),
-  },
-  ssr: false,
-  watchers: {
-    webpack: {
-      poll: true
-    }
-  },
-  vite: {
-    server: {
-      watch: {
-        usePolling: true,
-      },
-    },
-  },
+  modules: ['@nuxt/ui'],
 })
