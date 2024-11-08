e36d45405cb15b497f424daf0bd15ae4fb400692
sintaro-katuta <sintaro.katuta@gmail.com>
2024-11-08 11:10:58 +0900

    全フォルダのnode_modulesを管理対象から除外

--- front/pulse/nuxt.config.ts
+++ front/pulse/nuxt.config.ts
@@ -3,5 +3,15 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   extends: ['@nuxt/ui-pro'],
+<<<<<<< Updated upstream
   modules: ['@nuxt/ui'],
+=======
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/content',
+  ],
+  alias: {
+    '@': resolve(__dirname, './components'),
+  },
+>>>>>>> Stashed changes
 })
