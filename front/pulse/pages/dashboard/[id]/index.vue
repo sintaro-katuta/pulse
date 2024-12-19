4acf3a2a5797710bb77e47c802083b77dc09600f
sintaro-katuta <sintaro.katuta@gmail.com>
2024-12-20 01:09:57 +0900

    prismaの設定 #3

---
+++ front/pulse/pages/dashboard/[id]/index.vue
@@ -0,0 +1,42 @@
+<script setup lang="js">
+const route = useRoute()
+const id = route.params.id
+
+const projects = {
+  'id': 1,
+  'name': 'nazenaze',
+  'description': '2024前期チーム制作作品',
+  'icon': 'i-simple-icons-tailwindcss'
+}
+
+const links = [
+  {
+    label: 'Overview',
+    icon: 'i-heroicons-home',
+    to: `/dashboard/${id}`
+  },
+  {
+    label: 'Ticket',
+    icon: 'i-mdi-ticket-outline',
+    to: `/dashboard/${id}/tickets`,
+    badge: '3'
+  },
+  {
+    label: 'Settings',
+    icon: 'i-heroicons-cog',
+    to: `/dashboard/${id}/settings`
+  }
+]
+
+definePageMeta({
+  layout: 'dashboard'
+})
+</script>
+
+<template>
+  <div class="w-full h-full p-3 mx-3">
+    <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+    <UPageHeader :handline="projects.name" :title="projects.name" :description="projects.description"
+      :icon="projects.icon" />
+  </div>
+</template>
