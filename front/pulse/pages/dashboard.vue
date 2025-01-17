b59c78548e62b5ec8e9a10db53bef3e18d886b1c
勝田紳太郎 <113318875+sintaro-katuta@users.noreply.github.com>
2025-01-17 12:29:39 +0900

    Revert "概要画面結合"

---
+++ front/pulse/pages/dashboard.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const links = [{
+  label: 'Home',
+  icon: 'i-heroicons-home',
+  tooltip: {
+    text: 'Home',
+    shortcuts: ['G', 'H']
+  },
+}, {
+  label: 'Project',
+  icon: 'i-heroicons-folder-16-solid',
+  tooltip: {
+    text: 'Project',
+    shortcuts: ['G', 'P']
+  },
+  }, {
+  label: 'Settings',
+  icon: 'i-heroicons-cog-8-tooth',
+  children: [{
+    label: 'Profile',
+  }, {
+    label: 'Account',
+  }],
+  tooltip: {
+    text: 'Settings',
+    shortcuts: ['G', 'S']
+  },
+}]
+</script>
+
+<template>
+  <UDashboardLayout>
+    <UDashboardPanel>
+      <UDashboardNavbar />
+
+      <UDashboardSidebar>
+        <UDashboardSidebarLinks :links="links" />
+      </UDashboardSidebar>
+    </UDashboardPanel>
+
+    <slot />
+  </UDashboardLayout>
+</template>
